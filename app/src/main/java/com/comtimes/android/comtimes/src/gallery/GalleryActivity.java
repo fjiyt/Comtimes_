d6d0b65f8a8f69fbc21c9af2bfbb0dc0b278d412
mjlee0326 <mjlee032667@gmail.com>
2019-11-19 20:48:26 +0900

    gallery mypage 이민정

---
+++ app/src/main/java/com/comtimes/android/comtimes/src/gallery/GalleryActivity.java
@@ -0,0 +1,33 @@
+package com.example.comtimes;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GalleryActivity extends AppCompatActivity {
+    private int[] imageIDs = new int[] {
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image4,
+            R.drawable.image5,
+            R.drawable.image6,
+            R.drawable.image7,
+            R.drawable.image8,
+            R.drawable.image9,
+            R.drawable.image10,
+            R.drawable.image11
+    };
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+
+        GridView gridViewImages= findViewById(R.id.gridViewImages);
+        ImageGridAdapter imageGridAdapter=new ImageGridAdapter(this, imageIDs);
+        gridViewImages.setAdapter(imageGridAdapter);
+    }
+}
+
