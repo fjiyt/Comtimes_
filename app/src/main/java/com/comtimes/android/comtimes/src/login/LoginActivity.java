b0997dcfbb1498cdde314e9077138a9ccb9f726e
fjiyt <jy111099@gmail.com>
2019-11-24 04:20:20 +0900

    로그인 입력 예외처리 배지영

--- app/src/main/java/com/comtimes/android/comtimes/src/login/LoginActivity.java
+++ app/src/main/java/com/comtimes/android/comtimes/src/login/LoginActivity.java
@@ -1,15 +1,36 @@
 package com.comtimes.android.comtimes.src.login;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.util.Log;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import com.comtimes.android.comtimes.R;
 import com.comtimes.android.comtimes.src.BaseActivity;
+import com.comtimes.android.comtimes.src.register.RegisterActivity;
+
+import org.w3c.dom.ls.LSResourceResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.validation.Validator;
 
 
 public class LoginActivity extends BaseActivity {
@@ -21,18 +42,25 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mRegisterTv = findViewById(R.id.login_register_tv);
+        mRegisterTv.setOnClickListener(this);
         initViews();
+
       }
 
     @Override
     public void initViews() {
         mBackIbtn = findViewById(R.id.login_back_ibtn);
         mIdEt = findViewById(R.id.login_id_et);
+//        mIdEt.setFilters(new InputFilter[]{filter});
+
         mPwEt = findViewById(R.id.login_pw_et);
         mLoginIbtn = findViewById(R.id.login_login_ibtn);
         mFindIdTv = findViewById(R.id.login_find_id_tv);
         mFindPwTv = findViewById(R.id.login_find_pw_tv);
         mRegisterTv = findViewById(R.id.login_register_tv);
+
+
 
         mBackIbtn.setOnClickListener(this);
         mLoginIbtn.setOnClickListener(this);
@@ -43,12 +71,22 @@ public class LoginActivity extends BaseActivity {
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.login_login_ibtn:
+                if(!Pattern.matches("^[a-zA-Z0-9]*$", mIdEt.getText().toString())){
+                    Toast.makeText(LoginActivity.this,"아이디 형식을 지켜주세요.",Toast.LENGTH_SHORT).show();
+                }
+                if(!Pattern.matches("^[a-zA-Z0-9@!.#$%^&*_~()]*$", mPwEt.getText().toString())){
+                    Toast.makeText(LoginActivity.this,"비밀번호 형식을 지켜주세요.",Toast.LENGTH_SHORT).show();
+                }
                 break;
+
             case R.id.login_back_ibtn:
                 finish();
                 break;
+            case R.id.login_register_tv:
+                Intent intent = new Intent(this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+
         }
     }
-
-
 }
